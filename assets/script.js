afb742112c93aaf077c5abe82a6f9a58c7b751cf
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2022-10-30 13:57:29 -0700

    added logic for 'tasks' saved to local storage

--- assets/script.js
+++ assets/script.js
@@ -4,17 +4,21 @@ $("#currentDay").html(todayDate);
 
 $(document).ready(function () {
 // set up time based color coding logic for past present and future
-function getTime(blockhour) {
-    var currentHour = moment().hours();
-    if (blockhour < currentHour) return 'past';
-    if (blockhour === currentHour) return 'present';
-    if (blockhour > currentHour) return 'future';
-}
-// log just the number and not 'hour' 
-    $('.time-block').each(function () {
-        var blockhour = parseInt($(this).attr('id').split('-')[1]);
-// loop through timeblocks to determine class to call   
-        switch (getTime(blockhour)) {
+// set variable based on current time compared to timeblocks 
+    function getTime(blockHour) {
+        var currentHour = moment().hours();
+        if (blockHour < currentHour) return 'past'
+        if (blockHour === currentHour) return 'present'
+        if (blockHour > currentHour) return 'future'
+    }
+
+    function hourUpdater() {
+    // log just the number and not 'hour' 
+        $('.time-block').each(function () {
+            var blockhour = parseInt($(this).attr('id').split('-')[1]);
+    // loop through timeblocks to determine class to call   
+    console.log()
+            switch (getTime(blockHour)) {
             case 'past':
                 $(this).addClass('past');
                 break;
@@ -27,13 +31,17 @@ function getTime(blockhour) {
                 $(this).removeClass('present');
                 $(this).addClass('future');
                 default:
-                break;
-
+                break
+            }
+        });
+    }
 
 
 // logic of button click event 
-
+    $('.saveBtn').on('click', function () {
+        var value = $(this).siblings('.tasks').val();
+        var time = $(this).parent().attr('id');
 // save in local storage
-
-// set up time based color coding logic
-}
+        localStorage.setItem(time, value);
+    });
+});
