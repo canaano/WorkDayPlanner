8409dc45042ce31cfb34514db9491542c65411e7
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2022-10-30 10:38:58 -0700

    added current date in JS and Linked JS to HTML

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
 // show current time and date
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
 
+$(document).ready(function () {
 // logic of button click event 
 
 // save in local storage
 
 // set up time based color coding logic
 
-// 
+});
